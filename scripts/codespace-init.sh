16f90af09a60172da9442c19f0c524797c0f5e94
Sam Sartipi <sam.sartipi@clio.com>
2022-02-18 12:34:47 -0500

    test

--- scripts/codespace-init.sh
+++ scripts/codespace-init.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [[ "${CODESPACES:-}" == 'true' ]]; then
 
-  # change shell to zsh
+  set -x
+  echo $(whoami)
+  echo "change shell to zsh"
   sudo chsh -s $(which zsh) $(whoami)
   export SHELL="/bin/zsh"
+  set +x
 
-  # install starship prompt
+  echo "install starship prompt"
   sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
 
 fi
